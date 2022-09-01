77b2a595ef7de0422a801c3246c9df208514302b
Nicolas <nszchagas@gmail.com>
2022-09-01 17:22:40 -0300

    feat: authentication endpoint.

--- src/routers/auth.py
+++ src/routers/auth.py
@@ -1,15 +1,16 @@
 import re
-
-from fastapi import APIRouter, Depends
+import jwt
+from fastapi import APIRouter, Depends, status
 from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from passlib.context import CryptContext
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
-
 from database import get_db
 from models import User
 
 router = APIRouter()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 class CredentialsTemplate(BaseModel):
@@ -19,18 +20,11 @@ class CredentialsTemplate(BaseModel):
     class Config:
         schema_extra = {
             "example": {
-                "credential": "fulano",
-                "password": "fulano123"
+                "credential": "Fulano",
+                "password": "Problema123"
             }
 
         }
-
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
 
 
 @router.post("/auth", tags=["Authentication"])
@@ -46,9 +40,37 @@ async def auth_user(data: CredentialsTemplate, db: Session = Depends(get_db)):
         user: User = db.query(User).filter(
             User.username == data.credential, User.active == True).one_or_none()  # noq 712
     if not user:
-        return {"erro": True, "message": "Usuário não encontrado"}
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content={
+                "message": "Usuário não cadastrado.",
+                "error": True,
+                "data": []
+            },
+        )
     else:
-        v = pwd_context.verify(hash=user.password, secret=data.password)
-        return JSONResponse({
-            "valid": v
-        })
+        valid = pwd_context.verify(hash=user.password, secret=data.password)
+        if valid:
+            encoded = jwt.encode({
+                "username": user.username,
+                "name": user.name,
+                "job_role": user.job_role,
+                "access": user.acess
+            }, key="schedula", algorithm="HS256")
+            token = jsonable_encoder({'token': encoded})
+            {"username": "usuario1", "name": "Fulano",
+                "job_role": "Policial", "access": "admin"}
+
+            return JSONResponse(status_code=status.HTTP_200_OK,
+                                content={
+                                    "message": "Autenticação efetuada com sucesso.",
+                                    "error": None,
+                                    "data": token
+                                },)
+        else:
+            return JSONResponse(status_code=status.HTTP_200_OK,
+                                content={
+                                    "message": "Senha inválida.",
+                                    "error": True,
+                                    "data": []
+                                },)
