9f2da8914cc01ba1ec0af90d6c127b14a18612fa
Nicolas <nszchagas@gmail.com>
2022-09-04 19:30:54 -0300

    Updates in tests for post user endpoint.

--- tests/test_post_user.py
+++ tests/test_post_user.py
@@ -1,4 +1,6 @@
 import os
+from fastapi.testclient import TestClient
+from requests.structures import CaseInsensitiveDict
 
 user = {
     'username': 'teste',
@@ -11,11 +13,21 @@ user = {
 }
 
 
-def test_post_user(client):
+ADMIN_HEADER = CaseInsensitiveDict(
+    data={"Cookie": 'Authorization=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6Im1hbmFnZXIiLCJuYW1lIjoiRnVsYW5vIGRlIFRhbCIsImpvYl9yb2xlIjoiRXN0YWdpYXJpbyIsImFjY2VzcyI6ImFkbWluIn0.vu3T9_4xAf2UWL8n4c-Wm3pM8JZTAmwdBubrFWgX7nM'})
+MANAGER_HEADER = CaseInsensitiveDict(
+    data={"Cookie": 'Authorization=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6Im1hbmFnZXIiLCJuYW1lIjoiRnVsYW5vIGRlIFRhbCIsImpvYl9yb2xlIjoiRXN0YWdpYXJpbyIsImFjY2VzcyI6Im1hbmFnZXIifQ.zftUNuBvt8G19eq0Wqvnd52wBuxzIatQLcSpwIrWkUQ'})
+BASIC_HEADER = CaseInsensitiveDict(
+    data={"Cookie": 'Authorization=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6Im1hbmFnZXIiLCJuYW1lIjoiRnVsYW5vIGRlIFRhbCIsImpvYl9yb2xlIjoiRXN0YWdpYXJpbyIsImFjY2VzcyI6ImJhc2ljIn0.YOEKPNoyA5xK0X4R1z3KNB-v9E2Oy1AokmzArx-2bks'})
 
+# As admin
+
+
+def test_post_user_as_admin(client: TestClient):
     response = client.post(
-        "/user",
+        url="/user",
         json=user,
+        headers=ADMIN_HEADER
     )
     assert response.status_code == 201
     da = response.json()
@@ -24,12 +36,104 @@ def test_post_user(client):
     assert data["job_role"] == "trabalho 1"
 
 
-def test_post_user_used_email(client):
+def test_post_user_used_email_as_admin(client: TestClient):
     user['username'] = 'teste255'
     user['email'] = 'email1@email.com'
     response = client.post(
         "/user",
         json=user,
+        headers=ADMIN_HEADER
     )
     assert response.status_code == 409
     assert response.json()['message'] == "O email já está em uso"
+
+
+def test_post_user_username_as_admin(client: TestClient):
+    user['username'] = 'user_A'
+    user['email'] = 'email50@email.com'
+    response = client.post(
+        "/user",
+        json=user,
+        headers=ADMIN_HEADER
+    )
+    assert response.status_code == 409
+    assert response.json()['message'] == "O username já está em uso"
+
+# As manager
+
+
+def test_post_user_as_manager(client: TestClient):
+    user["username"] = "teste2"
+    user["email"] = "hsuh@email.com"
+    response = client.post(
+        "/user",
+        json=user,
+        headers=MANAGER_HEADER
+    )
+    assert response.status_code == 201
+    da = response.json()
+    data = da["data"]
+    assert data["username"] == "teste2"
+    assert data["job_role"] == "trabalho 1"
+
+
+def test_post_user_used_email_as_manager(client: TestClient):
+    user['username'] = 'teste255'
+    user['email'] = 'email1@email.com'
+    response = client.post(
+        "/user",
+        json=user,
+        headers=MANAGER_HEADER
+    )
+    assert response.status_code == 409
+    assert response.json()['message'] == "O email já está em uso"
+
+
+def test_post_user_username_as_manager(client: TestClient):
+    user['username'] = 'user_A'
+    user['email'] = 'email50@email.com'
+    response = client.post(
+        "/user",
+        json=user,
+        headers=MANAGER_HEADER
+    )
+    assert response.status_code == 409
+    assert response.json()['message'] == "O username já está em uso"
+
+# As basic
+
+
+def test_post_user_as_basic(client: TestClient):
+    user["username"] = "teste3"
+    user["email"] = "sahuisha@email.com"
+    response = client.post(
+        "/user",
+        json=user,
+        headers=BASIC_HEADER
+    )
+    assert response.status_code == 401
+    assert response.json()["message"] == "Acesso negado"
+
+
+def test_post_user_used_email_as_manager(client: TestClient):
+    user['username'] = 'teste255'
+    user['email'] = 'email1@email.com'
+    response = client.post(
+        "/user",
+        json=user,
+        headers=BASIC_HEADER
+    )
+    assert response.status_code == 401
+    assert response.json()["message"] == "Acesso negado"
+
+
+def test_post_user_username_as_manager(client: TestClient):
+    user['username'] = 'user_A'
+    user['email'] = 'email50@email.com'
+    response = client.post(
+        "/user",
+        json=user,
+        headers=BASIC_HEADER
+    )
+    assert response.status_code == 401
+    assert response.json()["message"] == "Acesso negado"
