298bf4d8d8f88732ee44e68f88b2e6d85a86ac77
Nicolas <nszchagas@gmail.com>
2022-09-04 18:25:18 -0300

    Changing middleware to main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 import time
-from urllib import response
 
-from fastapi import FastAPI, Request, Header
+from fastapi import FastAPI, Request
 from starlette.middleware.cors import CORSMiddleware
 
 from routers import auth, user
@@ -18,10 +17,17 @@ app.add_middleware(
 app.include_router(user.router)
 app.include_router(auth.router)
 
+
+@app.middleware("http")
+async def process_request_headers(request: Request, call_next):
+
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process time"] = str(process_time)
+    return response
+
+
 @app.get("/")
 def root():
     return {"APP": "Gestor de usuários is running"}
-
-@app.get("/items/")
-async def read_items(user_agent: str | None = Header(default=None)):
-    return {"User-Agent": user_agent}
