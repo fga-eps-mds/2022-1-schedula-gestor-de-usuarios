090080cbdacb44d9711ce3f14b1dfc90e47e70fa
Matheus Gabriel <matheuscoding@gmail.com>
2022-09-05 00:07:07 -0300

    Develop to main (#28)
    * feat: adding auth route.
    
    Co-authored-by: Nicolas <nszchagas@gmail.com>
    Co-authored-by: Samuel Nogueira Bacelar <samuelnbacelar@hotmail.com>

--- tests/data/insert_user.sql
+++ tests/data/insert_user.sql
@@ -1,12 +1,12 @@
 insert into user(username, job_role, name, email, password, active, acess, updated_at) values 
-('User A','Trabalho 1', 'Nome A', 'email 1', 'senha1', true, 'basic', '2020-01-01 00:00:00'),
-('User B','Trabalho 2', 'Nome A', 'email 2', 'senha2', true, 'basic', '2020-01-01 00:00:00'),
-('User C','Trabalho 3', 'Nome A', 'email 3', 'senha3', true, 'basic', '2020-01-01 00:00:00'),
-('User D','Trabalho 4', 'Nome A', 'email 4', 'senha4', true, 'basic', '2020-01-01 00:00:00'),
-('User E','Trabalho 5', 'Nome A', 'email 5', 'senha5', true, 'basic', '2020-01-01 00:00:00'),
-('User F','Trabalho 6', 'Nome A', 'email 6', 'senha6', true, 'basic', '2020-01-01 00:00:00'),
-('User G','Trabalho 7', 'Nome A', 'email 7', 'senha7', true, 'basic', '2020-01-01 00:00:00'),
-('User H','Trabalho 8', 'Nome A', 'email 8', 'senha8', true, 'basic', '2020-01-01 00:00:00'),
-('User I','Trabalho 9', 'Nome A', 'email 9', 'senha9', true, 'basic', '2020-01-01 00:00:00'),
-('User J','Trabalho 10', 'Nome A','email 10', 'senha10', true, 'basic', '2020-01-01 00:00:00'),
-('User K','Trabalho 11', 'Nome A','email 11', 'senha11', true, 'basic', '2020-01-01 00:00:00');
+('user_A','Trabalho 1', 'Nome A', 'email1@email.com', '$2b$12$lwmIAGrr/7SNAyRWEpA1Nu.N5qx4QUMUN8pKocYv8iP5TahRwRkk2', true, 'admin', '2020-01-01 00:00:00'),
+('user_B','Trabalho 2', 'Nome A', 'email2@email.com', 'senha2', true, 'admin', '2020-01-01 00:00:00'),
+('user_C','Trabalho 3', 'Nome A', 'email3@email.com', '$2b$12$RhyH5/fji2j3/he9mZ35JeY2wjFcw/CW.2IcaIh48RMwtRJSYEDvC', true, 'manager', '2020-01-01 00:00:00'),
+('user_D','Trabalho 4', 'Nome A', 'email4@email.com', 'senha4', true, 'manager', '2020-01-01 00:00:00'),
+('user_E','Trabalho 5', 'Nome A', 'email5@email.com', 'senha5', true, 'basic', '2020-01-01 00:00:00'),
+('user_F','Trabalho 6', 'Nome A', 'email6@email.com', 'senha6', true, 'basic', '2020-01-01 00:00:00'),
+('user_G','Trabalho 7', 'Nome A', 'email7@email.com', 'senha7', true, 'basic', '2020-01-01 00:00:00'),
+('user_H','Trabalho 8', 'Nome A', 'email8@email.com', 'senha8', false, 'basic', '2020-01-01 00:00:00'),
+('user_I','Trabalho 9', 'Nome A', 'email9@email.com', 'senha9', true, 'basic', '2020-01-01 00:00:00'),
+('user_J','Trabalho 10', 'Nome A','email10@email.com0', 'senha10', false, 'basic', '2020-01-01 00:00:00'),
+('user_K','Trabalho 11', 'Nome A','email11@email.com1', 'senha11', false, 'basic', '2020-01-01 00:00:00');
